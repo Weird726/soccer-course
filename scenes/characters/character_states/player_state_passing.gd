93bad6d4462a15802bd1dc43b5f75a1087d1d5b4
Weird726 <1293503189@qq.com>
2025-08-14 01:00:45 +0800

    added passing to teammates

--- scenes/characters/character_states/player_state_passing.gd
+++ scenes/characters/character_states/player_state_passing.gd
@@ -11,11 +11,12 @@ func _enter_tree() -> void:
 func on_animation_complete() -> void:
 	#寻找传球的目标
 	var pass_target := find_teammate_in_view()
-	print(pass_target)
-	#传入一个目标向量
-	var target := Vector2(10, 10)
-	#调用pass_to来使用这个方向向量,然后传入上面的速度
-	ball.pass_to(target)
+	#判断传球目标是否为空
+	if pass_target == null:
+		#没有目标的情况下，球只向前传一点
+		ball.pass_to(ball.position + player.heading * player.speed)
+	else:
+		ball.pass_to(pass_target.position + pass_target.velocity)
 	transition_state(Player.State.MOVING)
 
 #创建返回一个玩家的方法
